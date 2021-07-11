d981864f685e0e02c6a8a6c7178cb78e4d2be476
Manjaka <manjaka.rajaonson@gmail.com>
2021-07-11 20:42:45 +0300

    graphql base server

--- database.js
+++ database.js
@@ -8,34 +8,39 @@ class Database {
 			password: process.env.DATABASE_PASSWORD,
 			database: process.env.DATABASE_NAME,
 		});
-		this.err = null;
 	}
 
 	connect(callback) {
 		this.db.connect((err) => {
 			let status = false;
-			if (err) this.err = err;
-			else status = true;
+			if (!err) status = true;
+			else console.error(err);
 			if (typeof callback === "function") callback(status);
 		});
 	}
 
 	request(request) {
-		return new Promise((resolve, reject) => {
-			this.connect((status) => {
-				if (status) {
-					this.db.query(request, (err, res, fields) => {
-						this.disconnect();
-						if (err) reject(err);
-						else resolve(res);
-					});
-				} else reject("Unable to connect to database.");
+		if (typeof request === "string")
+			return new Promise((resolve, reject) => {
+				/* this.connect((status) => {
+					if (status) {
+						
+					} else reject("Unable to connect to database.");
+				}); */
+				this.db.query(request, (err, res, fields) => {
+					//this.disconnect();
+					if (err) reject(err);
+					else resolve(res);
+				});
 			});
-		});
+		else
+			return new Promise((resolve, reject) => {
+				reject("Unable to issue an empty request.");
+			});
 	}
 
 	disconnect(callback) {
-		this.db.end(callback);
+		this.db.destroy(callback);
 	}
 }
 
--- index.js
+++ index.js
@@ -1,39 +1,49 @@
+//Import .env file
 require("dotenv").config();
-const Express = require("express");
-const cors = require("cors");
+
+//Import necessary modules
 const { ApolloServer, gql } = require("apollo-server");
 const Database = require("./database.js");
+const auth = require("./auth.js");
 
-const app = Express();
+//Define usefull vars
 const port = process.env.PORT || 3001;
 const host = process.env.HOST || "http://127.0.0.1";
 const database = new Database();
 
-//Middlewares
-app.use(cors());
-app.use(Express.urlencoded({ extended: true }));
-app.use(Express.json());
-
+//Define our schemas
 const typeDefs = gql`
+	#User type
 	type User {
-		id: String!
-		name: String!
-		password: String!
+		id: String
+		name: String
 	}
+	#Query type
 	type Query {
 		users: [User!]
+		user(id: String!): User
 	}
 `;
 
+//Resolve our schemas
 const resolvers = {
 	Query: {
+		//Get list of all users
 		users: async () => {
-			const user_list = await database.request("SELECT * FROM users");
-			return user_list;
+			const result = await database.request(`SELECT * FROM users`);
+			return result;
+		},
+		//Get a specific user
+		user: async (parent, args, ctx) => {
+			const result = await database.request(
+				`SELECT * FROM users WHERE id="${args.id}"`
+			);
+			return result[0];
 		},
 	},
 };
 
+//Run the apollo server
 const apollo_server = new ApolloServer({ typeDefs, resolvers });
 apollo_server
 	.listen({
