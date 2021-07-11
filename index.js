1e1ef43fa5cf1f1094db04f0e5f0efa40466d200
Manjaka <manjaka.rajaonson@gmail.com>
2021-07-11 19:18:00 +0300

    get apollo server ready

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 require("dotenv").config();
 const Express = require("express");
 const cors = require("cors");
+const { ApolloServer, gql } = require("apollo-server");
+const Database = require("./database.js");
 
 const app = Express();
 const port = process.env.PORT || 3001;
+const host = process.env.HOST || "http://127.0.0.1";
+const database = new Database();
 
 //Middlewares
 app.use(cors());
-app.use(Express.urlencoded({extended: true}));
+app.use(Express.urlencoded({ extended: true }));
 app.use(Express.json());
 
-app.get("/", (req, res) => {
-	res.json("Autocontrolbox webservice.");
-});
+const typeDefs = gql`
+	type User {
+		id: String!
+		name: String!
+		password: String!
+	}
+	type Query {
+		users: [User!]
+	}
+`;
 
-//Listen
-app.listen(port, () => {
-	console.log("Autocontrolbox webservice running on port " + port);
-});
+const resolvers = {
+	Query: {
+		users: async () => {
+			const user_list = await database.request("SELECT * FROM users");
+			return user_list;
+		},
+	},
+};
+
+const apollo_server = new ApolloServer({ typeDefs, resolvers });
+apollo_server
+	.listen({
+		port,
+		url: host,
+	})
+	.then(() => {
+		console.log(`🚀 Autocontrolbox webservice ready at ${host}:${port}`);
+	});
